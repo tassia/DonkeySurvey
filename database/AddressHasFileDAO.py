10220ec057a445affc067d2c380f6fe988634333
Vinicius Gama <vinnie.gp@gmail.com = vinicius vinicius@ime.usp.br>
2009-12-08 17:34:36 +0000

    small bug

--- database/AddressHasFileDAO.py
+++ database/AddressHasFileDAO.py
@@ -20,7 +20,7 @@ class AddressHasFileDAO(CommonDAO):
 	        query = "INSERT INTO %s(address_id, file_id, first_seen) VALUES(%s, %s,'%s')" % (self.jointable, addressId, fileId, firstSeen)
                 logging.debug(query)
                 self.cursor.execute(query)
-                last = CommonDAO.lastID(self, self.tablename)
+                last = CommonDAO.lastID(self, self.jointable)
                 return last 
         except Exception, err:
             sys.stderr.write('ERROR: %s\n' % str(err))
